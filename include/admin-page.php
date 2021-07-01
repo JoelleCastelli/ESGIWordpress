d363d4404b977a74dc1b33934e377be14cca45b4
Joelle <castelli.jo@gmail.com>
2021-07-01 15:53:37 +0200

    config form

--- include/admin-page.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo "<div class='wrap'>
-<h1>Config du plugin</h1>
-</div>";
--- include/functions.php
+++ include/functions.php
@@ -1,19 +1,76 @@
 <?php
 
 // TEST
-add_action('wp_footer', 'grp1_FooterAddText');
-function grp1_FooterAddText() {
+add_action('wp_footer', 'esgi_tmdb_FooterAddText');
+function esgi_tmdb_FooterAddText() {
     echo "hello";
 }
 
 // Add menu in admin list
-add_action('admin_menu', 'grp1_addAdminLink');
-function grp1_addAdminLink() {
+add_action('admin_menu', 'esgi_tmdb_addAdminLink');
+function esgi_tmdb_addAdminLink() {
     add_menu_page(
-        'ESGI TMDB',
-        'ESGI TMDB',
-        'manage_options',
-        'esgi_tmdb',
-        'esgi-tmdb/include/admin-page.php'
+        'Configuration du plugin ESGI TMDB',  // Page title
+        'ESGI TMDB',                         // Menu title
+        'manage_options',                    // Capability
+        'esgi-tmdb',                        // Slug
+        'esgi_tmdb_config_page',             // Display callback
+        'dashicons-pets'                     // Icon
     );
+}
+
+// Config page layout
+function esgi_tmdb_config_page() {
+    ?>
+    <div class="wrap">
+        <h1><?= esc_html(get_admin_page_title()) ?></h1>
+        <div class="wrap">
+            Pour obtenir votre clé API, consultez la <a href="https://developers.themoviedb.org/3/getting-started/introduction" target="_blank">documentation TMDB</a>.
+            <form action="options.php" method="POST">
+                <?php
+                settings_fields('esgi_tmdb_settings');
+                do_settings_sections('esgi_tmdb_settings_page');
+                submit_button();
+                ?>
+            </form>
+        </div>
+    </div>
+    <?php
+}
+
+// Register a setting
+add_action('admin_init', 'esgi_tmdb_settings');
+function esgi_tmdb_settings(){
+    register_setting(
+        'esgi_tmdb_settings',  // Group name
+        'esgi_tmdb_settings',  // Setting name
+        'esgi_tmdb_sanitize'                   // Callback function
+    );
+
+    add_settings_section(
+        'esgi_tmdb_config_section',  // ID
+        '',                         // Title
+        '',                      // Callback
+        'esgi_tmdb_settings_page'  // Page
+    );
+
+    add_settings_field(
+        'public-tmdb-api-key',         // ID
+        'Clé publique API TMDB',      // Title
+        'esgi_tmdb_display_field',           // Callback
+        'esgi_tmdb_settings_page',   // Page
+        'esgi_tmdb_config_section'  // Section ID
+    );
+}
+
+// Display the field
+function esgi_tmdb_display_field(){
+    $setting = get_option('esgi_tmdb_settings');
+    $value = !empty($setting['text']) ? $setting['text'] : '';
+    echo '<input class="regular-text" type="text" name="esgi_tmdb_settings[text]" value="'.esc_attr($value).'">';
+}
+
+function esgi_tmdb_sanitize( $settings ){
+    $settings['text'] = ! empty( $settings['text'] ) ? sanitize_text_field( $settings['text'] ) : '';
+    return $settings;
 }
